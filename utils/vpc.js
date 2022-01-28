41c206dff04cca88b908202649d19dab416f1f1c
DiscreteTom <discrete_tom@outlook.com>
2022-01-28 15:46:32 +0800

    add vpc.checkNacl/checkSubnetNacl

--- utils/vpc.js
+++ utils/vpc.js
@@ -88,7 +88,118 @@ async function getSubnetRouteTable({ $, subnetId, vpcId }) {
   }
 }
 
+/**
+ * ## Params
+ *
+ * - `$`: context
+ * - `subnetId`
+ * - `direction`: `'in'`/`'out'`
+ * - `protocol`: protocol number(e.g. '17' means udp, '6' means tcp) or tcp/udp/icmp
+ * - `port`: e.g. `22`
+ *
+ * ## Return
+ *
+ * ```
+ * {
+ *   any: false,
+ *   rule: [{
+ *     allow: true,
+ *     cidr: '0.0.0.0/0',
+ *     number: 100,
+ *   }],
+ * }
+ * ```
+ */
+async function checkSubnetNacl({ $, subnetId, direction, protocol, port }) {
+  // translate literal protocol to number
+  switch (protocol) {
+    case "tcp":
+      protocol = "6";
+      break;
+    case "udp":
+      protocol = "17";
+      break;
+    case "icmp":
+      protocol = "1";
+      break;
+  }
+
+  let res = await $.aws.ec2.describeNetworkAcls({
+    Filters: [{ Name: "association.subnet-id", Values: [subnetId] }],
+  });
+
+  return checkNacl({ $, res, direction, protocol, port });
+}
+
+/**
+ * ## Params
+ *
+ * - `$`: context
+ * - `res`: the response of `describeNetworkAcls`
+ * - `direction`: `'in'`/`'out'`
+ * - `protocol`: protocol number, e.g. '17' means udp, '6' means tcp
+ * - `port`: e.g. `22`
+ *
+ * ## Return
+ *
+ * ```
+ * {
+ *   any: false,
+ *   rule: [{
+ *     allow: true,
+ *     cidr: '0.0.0.0/0',
+ *     number: 100,
+ *   }],
+ * }
+ * ```
+ */
+function checkNacl({ $, res, direction, protocol, port }) {
+  let rules = $.jp
+    .query(res, `$..Entries[?(${direction == "in" ? "!" : ""}@.Egress)]`)
+    .filter((r) => {
+      // filter protocol & port
+      if (r.Protocol == "-1") {
+        // any traffic
+        return true;
+      } else if (r.Protocol == protocol) {
+        // icmp
+        if (protocol == "1" && r.IcmpTypeCode.Type == port) {
+          return true;
+        } else if (r.PortRange.From <= port && r.PortRange.To >= port) {
+          return true;
+        }
+      }
+      return false;
+    })
+    .sort((a, b) => a.RuleNumber - b.RuleNumber);
+
+  let result = {
+    any: false,
+    rule: [],
+  };
+
+  for (let i = 0; i < rules.length; i++) {
+    const r = rules[i];
+    result.rule.push({
+      allow: r.RuleAction == "allow",
+      cidr: r.CidrBlock,
+      number: r.RuleNumber,
+    });
+  }
+
+  if (result.rule.length > 0) {
+    let r = result.rule[0];
+    if (r.allow && r.cidr == "0.0.0.0/0") {
+      result.any = true;
+    }
+  }
+
+  return result;
+}
+
 export default {
   checkSubnetIgw,
   getSubnetRouteTable,
+  checkSubnetNacl,
+  checkNacl,
 };
