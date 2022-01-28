27f6e07152197654b2c13ba52a028a626335ab1a
DiscreteTom <discrete_tom@outlook.com>
2022-01-28 17:50:39 +0800

    more aws sdk

--- aws.js
+++ aws.js
@@ -1,9 +1,21 @@
+import { CloudFront } from "@aws-sdk/client-cloudfront";
 import { EC2 } from "@aws-sdk/client-ec2";
-import { RDS } from "@aws-sdk/client-rds";
 import { ElasticLoadBalancingV2 } from "@aws-sdk/client-elastic-load-balancing-v2";
+import { ElastiCache } from "@aws-sdk/client-elasticache";
+import { GameLift } from "@aws-sdk/client-gamelift";
+import { Lambda } from "@aws-sdk/client-lambda";
+import { RDS } from "@aws-sdk/client-rds";
 
 // init `aws` with all client sdk
-let aws = { EC2, RDS, ElasticLoadBalancingV2 };
+let aws = {
+  CloudFront,
+  EC2,
+  ElasticLoadBalancingV2,
+  ElastiCache,
+  GameLift,
+  Lambda,
+  RDS,
+};
 
 function configure({ accessKeyId, secretAccessKey, region }) {
   let config = {
@@ -17,9 +29,13 @@ function configure({ accessKeyId, secretAccessKey, region }) {
     };
   }
   // init all clients
+  aws["cloudfront"] = new CloudFront(config);
   aws["ec2"] = new EC2(config);
-  aws["rds"] = new RDS(config);
   aws["elbv2"] = new ElasticLoadBalancingV2(config);
+  aws["elasticache"] = new ElastiCache(config);
+  aws["gamelift"] = new GameLift(config);
+  aws["lambda"] = new Lambda(config);
+  aws["rds"] = new RDS(config);
 }
 
 aws["configure"] = configure;
